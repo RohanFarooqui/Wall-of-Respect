b95ed0b067c64f0336a268d354345ffd8bbb6e25
M.ROHAN FAROOQUI <31238460+RohanFarooqui@users.noreply.github.com>
2021-08-10 02:20:51 +0500

    API | Users

--- API/app.py
+++ API/app.py
@@ -8,6 +8,26 @@ from flask                 import Flask ,url_for,request,jsonify
 from   datetime import datetime
 import emoji
 
+# => File Imports
+
+# ==> Database Py Files
+from Database_py.Tbl_Users import *
+
+# ==> Api Py Files
+from Api_py.Api_Users import *
+
+
+
+# => Database Connection 
+try:
+    DB_Con_1 =  table_users('sql6.freemysqlhosting.net','sql6437945','isxEqZIuys','sql6437945')      
+    #DB_Con_2 =  table_roles('sql6.freemysqlhosting.net','sql6437945','isxEqZIuys','sql6437945')      
+    #DB_Con_3 =  table_associates('sql6.freemysqlhosting.net','sql6437945','isxEqZIuys','sql6437945') 
+    #DB_Con_4 =  table_Campaign('sql6.freemysqlhosting.net','sql6437945','isxEqZIuys','sql6437945')
+    Database_Status = "Perfect !!"
+except:
+    Database_Status = "Connection Error !!"
+
 # => Flask 
 app = Flask(__name__)
 
@@ -22,15 +42,31 @@ class main:
         Msg = { "1.Name ": "Wall of Respect",
                 "2.Api Version": "1.0",
                 "3.Developed by" : "M.ROHAN FAROOQUI©",
-                "4.API Status" : "Running "+emoji.emojize(":grinning_face_with_big_eyes:"),}
-                #"5.Database Status " :  Database_Status }
+                "4.API Status" : "Running "+emoji.emojize(":grinning_face_with_big_eyes:"),
+                "5.Database Status " :  Database_Status }
         return jsonify(Msg),200
 
-
+    ########################### ==> User API <== ###########################
+    
+    ######## => Add , View & Update  <= ########
+    @app.route("/v1/user",methods=['GET','POST','PUT'])
+    def User():
+        if(request.method == 'POST' and 'Added_by' in request.args):
+            Result_1 = User(DB_Con_1).Add_User()
+            return Result_1
+        elif(request.method == 'POST' and 'Name' in request.args):  ### For Info Update
+            Result_2 = User(DB_Con_1).Update_User_info()
+            return Result_2
+        elif(request.method == 'POST' and 'Pswd' in request.args):  ### For Password Update
+            Result_3 = User(DB_Con_1).Update_User_Pswd()
+            return Result_3            
+        elif(request.method == 'GET'):
+            Result_4 = User(DB_Con_1).View_User()
+            return Result_4
 
 
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)  ## For Debug
-    app.run()
+    app.run(debug=True)  ## For Debug
+    #app.run()
