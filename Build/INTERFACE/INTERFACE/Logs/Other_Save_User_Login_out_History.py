872a358b42ed5a5b0139df306e724144d8341e76
M.ROHAN FAROOQUI <31238460+RohanFarooqui@users.noreply.github.com>
2021-08-26 16:47:32 +0500

    INTERFACE | Complete ..

---
+++ Build/INTERFACE/INTERFACE/Logs/Other_Save_User_Login_out_History.py
@@ -0,0 +1,66 @@
+##-> Imports
+
+#-> Lib Imports
+import csv
+import os
+
+#-> File Imports
+from INTERFACE.Other.Other_Get_Date_and_Time import * 
+
+
+class Save_User_Login_History:
+
+    def __init__(self):
+         self.File_name = get_date()+".csv"
+
+    def Check_Folder_exist(self):
+        try:
+            if(os.path.exists("Logs\\")):
+                return True
+            else:
+                if(os.path.exists("Logs")):
+                    os.mkdir(os.getcwd()+"\\Logs\\Login_Logout")
+                    return True
+                else:
+                    os.mkdir(os.getcwd()+"\\Logs")
+                    os.mkdir(os.getcwd()+"\\Logs\\Login_Logout")
+                    return True
+        except:
+            pass
+
+    def File_exist(self):
+        if(os.path.isfile("Logs\\Login_Logout\\"+self.File_name)):
+            return True
+        else:
+            return False
+
+    def Create_File_and_Write_Columns(self):
+        try:
+            if(self.Check_Folder_exist()):
+                if not self.File_exist() :
+                    Columns_Names = ["User_ID","User_Name","Operation","Time","Date"]
+                    with open("Logs\\Login_Logout\\"+self.File_name, 'w',encoding="utf-8") as f:
+                        write = csv.writer(f) 
+                        write.writerow( Columns_Names)  
+                return True
+        except:
+            pass
+               
+    def Write_message(self,msg):  
+        if(self.File_exist()):
+            with open("Logs\\Login_Logout\\"+self.File_name, 'a',newline='') as f:
+                write = csv.writer(f)
+                write.writerow(msg)
+                f.close()
+
+        else:
+            self.Create_File_and_Write_Columns()
+            self.Write_message(msg)
+
+        
+            
+
+
+     
+
+
---
+++ Build/INTERFACE/INTERFACE/Logs/Other_Save_User_Logs.py
@@ -0,0 +1,58 @@
+##-> Imports
+
+#-> Lib Imports
+import csv
+import os
+
+#-> File Imports
+from INTERFACE.Other.Other_Get_Date_and_Time import * 
+
+class Save_User_Logs_History:
+
+    def __init__(self):
+         self.File_name = get_date()+".csv"
+
+    def Check_Folder_exist(self):
+        try:
+            if(os.path.exists("Logs\\Crud_Operations")):
+                return True
+            else:
+                if(os.path.exists("Logs")):
+                    os.mkdir(os.getcwd()+"\\Logs\\Crud_Operations")
+                    return True
+                else:
+                    os.mkdir(os.getcwd()+"\\Logs")
+                    os.mkdir(os.getcwd()+"\\Logs\\Crud_Operations")
+                    return True
+        except:
+            pass
+
+    def File_exist(self):
+        if(os.path.isfile("Logs\\Crud_Operations\\"+self.File_name)):
+            return True
+        else:
+            return False
+
+    def Create_File_and_Write_Columns(self):
+        try:
+            if(self.Check_Folder_exist()):       
+                if not self.File_exist():
+                    Columns_Names = ["User_ID","User_Name","Operation Detail","Time","Date"]
+                    with open("Logs\\Crud_Operations\\"+self.File_name, 'w',encoding="utf-8") as f:
+                        write = csv.writer(f) 
+                        write.writerow( Columns_Names)  
+                return True
+        except:
+            pass
+               
+    def Write_message(self,msg): 
+        if(self.File_exist()):
+            with open("Logs\\Crud_Operations\\"+self.File_name, 'a',newline='') as f:
+                write = csv.writer(f)
+                write.writerow(msg)
+                write.flush()
+                f.close()
+        else:
+            self.Create_File_and_Write_Columns()
+            self.Write_message(msg)
+
