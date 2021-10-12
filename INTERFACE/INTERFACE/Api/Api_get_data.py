e1dbbf0a169fb62d3425ad48df991ae733b9e7a8
M.ROHAN FAROOQUI <31238460+RohanFarooqui@users.noreply.github.com>
2021-10-12 22:59:29 +0500

    Interface | UPDATE

--- INTERFACE/INTERFACE/Api/Api_get_data.py
+++ INTERFACE/INTERFACE/Api/Api_get_data.py
@@ -13,8 +13,8 @@ from INTERFACE.Template.Sessions import *
 
 
 
-default_api_url = "http://127.0.0.1:5000/"
-#default_api_url = "https://wall-of-respect-api.herokuapp.com/"
+#default_api_url = "http://127.0.0.1:5000/"
+default_api_url = "https://wall-of-respect-api.herokuapp.com/"
 
 
 ####################-> Dashboard <-####################
